0fc0b9858a9c54d2a8bce353f1082cd00498375d
Matteogatto94 <cosenza94@live.it>
2022-12-21 16:08:55 +0100

    put data in database

--- database/migrations/2022_12_21_143124_create_comics_table.php
+++ database/migrations/2022_12_21_143124_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->float('price', 4, 2)->nullable();
+            $table->string('series')->nullable();
+            $table->string('sale_date', 15)->nullable();
+            $table->string('type')->nullable();
             $table->timestamps();
         });
     }
